e203393135c091f9db12f6f281745a7d128de4f2
Jamis Charles <jamis82@webutah.com>
2023-02-27 15:23:33 -0700

    initial commit

---
+++ src/routes/[slug]/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+
+  export let data: {
+    slug: string;
+    post: string;
+    snippetStyle: string;
+  };
+</script>
+
+{@html `<style>${data.snippetStyle}</style>`}
+
+<h1>{data.slug}</h1>
+
+<p>{@html data.post}</p>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style>
